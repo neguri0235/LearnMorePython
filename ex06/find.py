13d6015c06e4af990814ed68344c94ab2b729583
neguri <neguri0235@gmail.com>
2019-09-21 22:28:18 +0900

    ex06

--- ex06/find.py
+++ ex06/find.py
@@ -1,34 +1,54 @@
-# find . -name "*.txt" -print
-# find . -name "*.rb" -exec rm {} \;
-
 import sys
 import os
 import glob
 import subprocess
+import argparse
+import logging
 
-def isHelp(string):
-    if string[1] == '--help' or string[1] == '-h':
-        print('find - search for files in a directory hierarchy')
-        return True
-    else:
-        return False
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s = %(levelname)s - %(message)s')
+logging.disable(logging.DEBUG)
 
-def parse(arg):
-    for e in arg:
+def matcher(name, file_list):
+    logging.debug("-------matched list -------")
+    logging.debug("matcher: %s" % name)
+    match_file_list = []
+    return match_file_list
+
+def traverse_dir(path):
+    logging.debug("-------file list -------")
+    all_file_list = []
+    for dir_name, sub_dir_name, file_list in os.walk(path):
+        logging.debug("base directory: %s" % dir_name)
+        for fname in file_list:
+            logging.debug(fname)
+    return all_file_list
+
+def using_glob(path,name):
+    base_condition = path+'/**/'+name
+    glob_list = glob.glob(base_condition,recursive=True)
+    logging.debug("-------glob list -------")
+    for e in glob_list:
+        logging.debug("%s" % e)
+    return glob_list    
+
+def print_result(result):
+    for e in result:
         print(e)
 
-def run(arg):
-    redirect = False
-    size = len(arg)
-
-    if size == 1:
-        print('do nothing')
-        return
-    elif size == 2:
-        if isHelp(arg) == True:
-            return
-    else:
-        parse(arg)
-
+def run(args):
+#    all_file_list = traverse_dir(args.path)
+#    match_file_list = matcher(args.name, all_file_list)
+    find_result = using_glob(args.path, args.name)
+    print_result(find_result)
+    
 if __name__ == "__main__":
-    run(sys.argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="base directory")
+    parser.add_argument("-name", help="file name or directory name")
+    parser.add_argument("-exec", help="execute")    
+    args = parser.parse_args()
+    logging.debug("-------argument-------")
+    logging.debug("path: %s" % args.path)
+    logging.debug("name: %s" % args.name)
+    logging.debug("exec: %s" % args.exec)
+    run(args)
